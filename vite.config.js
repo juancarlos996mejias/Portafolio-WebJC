dfe52e7a0779d1eb0d6ae47e4be9b2bcc8ef9c22
GeoSystem <juancarlos996@gmail.com>
2025-12-03 11:18:44 -0300

    Fix Vite HTTPS paths

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,25 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: process.env.VITE_BASE_URL ? process.env.VITE_BASE_URL + '/build/' : '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+
+    // Fuerza a Vite a generar URLs absolutas con HTTPS EN PRODUCCIÓN
+    base: process.env.VITE_BASE_URL || '/',
+
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        assetsDir: 'assets',
+    },
+
+    server: {
+        https: false, // local
+        host: true,
+    },
 });
+
