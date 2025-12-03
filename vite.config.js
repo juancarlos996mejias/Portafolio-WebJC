883c85bd5abe0e50e08ddf82492ffbacc3075852
GeoSystem <juancarlos996@gmail.com>
2025-12-03 11:14:16 -0300

    actualizando vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: process.env.VITE_BASE_URL ? process.env.VITE_BASE_URL + '/build/' : '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
